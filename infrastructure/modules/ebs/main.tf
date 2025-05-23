e64dce1e7a1b7be5eeb499df31b058c39f718f01
cryptoPickle <halilibrahimirmak@gmail.com>
2025-05-23 10:42:47 +0100

    small change to assing role to elasticbeanstalk

--- infrastructure/modules/ebs/main.tf
+++ infrastructure/modules/ebs/main.tf
@@ -1,11 +1,16 @@
 module "iam" {
   source             = "../iam_role/"
-  name               = "${var.name}-beanstalk-role"
+  name               = "${var.app_name}-beanstalk-role"
   assume_role_policy = "../iam/policies/beanstalk_assume_role.json"
   policies = templatefile("${path.module}../iam/policies/app_role.json", {
     docker_image_url = var.docker_image_url
   })
   tags = var.tags
+}
+
+resource "aws_iam_instance_profile" "this" {
+  name = "${var.app_name}-elasticbeanstalk-profile"
+  role = module.iam.role_arn
 }
 
 resource "aws_elastic_beanstalk_application" "this" {
@@ -18,6 +23,11 @@ resource "aws_elastic_beanstalk_environment" "this" {
   application         = aws_elastic_beanstalk_application.this.name
   solution_stack_name = "64bit Amazon Linux 2 v5.8.4 running Docker"
 
+  setting {
+    namespace = "aws:autoscaling:launchconfiguration"
+    name      = "IamInstanceProfile"
+    value     = aws_iam_instance_profile.this.name
+  }
   setting {
     namespace = "aws:elasticbeanstalk:container:docker"
     name      = "Image"
